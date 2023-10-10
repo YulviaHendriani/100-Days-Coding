a49bbd2472c4fce12045b2e9d7dd4f59cf5f25af
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-10 22:47:41 +1345

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,28 @@
+
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        
+        byte angka = 1;
+        short angka1 = 2;
+        int angka2 = 3;
+        long angka3 = 4;
+        float angka4 = 5.1f;
+        double angka5= 5.2;
+        char jk = 'P';
+        boolean status = true; 
+        
+        System.out.println(angka);
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+        System.out.println(angka4);
+        System.out.println(angka5);
+        System.out.println(jk);
+        System.out.println(status);
+        
+    }
+    
+}

429baa49d4649363f564eebff83fb6327328c560
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-11 22:19:38 +1345

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,15 @@
+
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        String nama = "Yulvia Hendriani";
+        String kelas = "Informatika A";
+       
+        System.out.println(nama);
+        System.out.println(kelas);
+        
+    }
+     
+}

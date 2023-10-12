d78be5769a83437464a12fabebfdfd5ebb0d98b2
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-13 00:28:03 +1345

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,12 @@
+
+package day4;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        String nama= "Yulvia";
+        byte umur= 19;
+        System.out.println("Nama\t: " + nama + "\n" + "Umur\t: " + umur);
+    }
+    
+}

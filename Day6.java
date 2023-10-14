c334a313b8bf650a940c33756be81d6bf190b248
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-14 23:28:14 +1345

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,13 @@
+
+package day6;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+       String nama= "Yulvia";
+       String prodi = "Teknik Informatika";
+       char kelas ='A';
+        System.out.println("Nama: " +nama + "\n" + "Prodi: " + prodi + "\n" + "Kelas: " + kelas);
+    }
+    
+}

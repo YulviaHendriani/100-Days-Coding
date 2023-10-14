41fc9fad0a4cbe33cb9864d96eefaf1e1bd7b001
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-15 06:01:55 +1345

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,15 @@
+
+package day7;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        
+        String nama = "Yulvia Hendriani";
+        float berat_badan = 46.5f;
+        double tinggi_badan = 155.2;
+        System.out.println("Nama\t\t:" + nama + "\n" + "Berat Badan\t:"  + berat_badan + "\n" + "Tinngi Badan\t:" + tinggi_badan);
+        
+    }
+    
+}

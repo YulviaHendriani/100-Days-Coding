361beab148e32bdc3d9fa9e730b91c4f8dc2ee1f
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-18 02:15:20 +1345

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,15 @@
+
+package day9;
+
+public class Day9 {
+
+   
+    public static void main(String[] args) {
+        
+        int nilai_1 = 83;
+        int nilai_2 = 80;
+        
+        System.out.println("Jumlah Nilai 1 dan 2: " + (nilai_1 + nilai_2));
+    }
+    
+}

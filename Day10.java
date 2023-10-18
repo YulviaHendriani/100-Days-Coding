4d2d3902104b07516dffec2fe86654ea714fbfc0
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-19 00:57:37 +1345

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,12 @@
+
+package day10;
+
+public class Day10 {
+    
+    public static void main(String[] args) {
+         int angka1 = 5;
+         int angka2 = 8;
+          System.out.println("angka1 x angka2= " + (angka1*angka2));
+    }
+    
+}

abf5882731bd91aa81a808931e6f2bb730e4b55a
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-27 01:08:00 +1345

    Add files via upload

---
+++ Day18.java
@@ -0,0 +1,12 @@
+
+package day18;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        
+        for (int b=3; b<12; b+=3){
+            System.out.println(b);
+    }
+  } 
+}

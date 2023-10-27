26b2a1097e71327bef1f86ae85ea9da00d852160
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-27 22:58:37 +1345

    Add files via upload

---
+++ Day19Perulangan.java
@@ -0,0 +1,13 @@
+
+package day19perulangan;
+
+public class Day19Perulangan {
+
+    public static void main(String[] args) {
+       
+        
+        for (int b=1; b<11; b++){
+            System.out.println(b+ " Yulvia");
+    }
+  } 
+}

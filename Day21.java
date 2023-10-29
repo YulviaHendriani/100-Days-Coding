40777b097e006b56e8db246e6ee069f94d26595e
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-30 03:06:27 +1345

    Add files via upload

---
+++ Day21.java
@@ -0,0 +1,11 @@
+
+package day21;
+public class Day21 {
+    public static void main(String[] args) {
+       
+         for (int a=1; a<10; a+=2){
+            System.out.println(a+1);
+        }
+    }
+    
+}

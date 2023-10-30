239c2354f2a63ec705e50b6a4e3b8bcdab3ba733
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-10-31 00:17:34 +1345

    Add files via upload

---
+++ Day22.java
@@ -0,0 +1,13 @@
+
+package day22;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        
+        for (int i=4; i<20; i++){
+            System.out.println("Yulvia");  
+        }
+    }
+    
+}

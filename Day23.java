474b302767468d13728e4617afbc6f6989a5fca5
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-01 01:30:16 +1345

    Add files via upload

---
+++ Day23.java
@@ -0,0 +1,14 @@
+
+package day23;
+
+import java.math.BigInteger;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+      
+        BigInteger a = new BigInteger("120523");
+        System.out.println(a);
+    }
+    
+}

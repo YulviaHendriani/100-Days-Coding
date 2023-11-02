341d3b1749bc94a8e78f079bc05502f1d2bb0500
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-02 19:57:24 +1345

    Add files via upload

---
+++ Day24.java
@@ -0,0 +1,13 @@
+
+package day24;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+        
+        for (int a=1; a<=12; a++){
+            System.out.print(a+",");
+        }
+    }
+    
+}
---
+++ Day25.java
@@ -0,0 +1,21 @@
+
+package day25;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+       
+        int a = 5;
+        System.out.println(a+=4); 
+        int b = 12;
+        System.out.println(b-=5);
+        int c = 10;
+        System.out.println(c*2);
+        int d = 20;
+        System.out.println(d/=5);
+        int e = 11;
+        System.out.println(e%=2);        
+                
+    }
+    
+}

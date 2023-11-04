9f5188515ff2b6491b4ac97fa63e0f5eb6d369bf
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-05 02:49:20 +1345

    Add files via upload

---
+++ Day27.java
@@ -0,0 +1,19 @@
+
+package day27;
+
+public class Day27 {
+
+    
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 12;
+         
+         if (a < b){
+          System.out.println("Benar");  
+         }else{ 
+          System.out.println("Salah"); 
+         }
+    }
+    
+}

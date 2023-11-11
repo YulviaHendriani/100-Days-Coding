b0c350159b5acf233b3db7c121dd8ab9814e93dc
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-12 03:26:59 +1345

    Add files via upload

---
+++ Day34.java
@@ -0,0 +1,13 @@
+
+package day34;
+
+public class Day34 {
+
+    public static void main(String[] args) {
+        
+        for(int y = 10; y >= 1; y -= 4){
+            System.out.println(y);
+        }
+    }
+    
+}

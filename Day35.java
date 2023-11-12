232620e54ffe9d8d1dbc96fbcda8285001f2a996
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-13 03:52:41 +1345

    Add files via upload

---
+++ Day35.java
@@ -0,0 +1,17 @@
+package day35;
+
+public class Day35 {
+
+    public static void main(String[] args) {
+       
+        double hasil, x, y;
+        
+        x = 6.0;
+        y = 3.0;
+        
+        hasil = (2*x)+(2*y);
+        
+        System.out.println(hasil);
+    }
+    
+}

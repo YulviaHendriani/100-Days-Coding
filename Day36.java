3e0c6adc11fbcbaa0d9e0621a1e615120f12f9b0
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-14 02:32:36 +1345

    Add files via upload

---
+++ Day36.java
@@ -0,0 +1,28 @@
+
+package day36;
+
+public class Day36 {
+
+    public static void main(String[] args) {
+    int a = 12;
+    int b = 23;
+    boolean hasil;
+     
+    hasil = a == b;
+    System.out.println("Apakah a == b ? " + hasil );   
+     
+    hasil = a != b;
+    System.out.println("Apakah a != b ? " + hasil );   
+     
+    hasil = a > b;
+    System.out.println("Apakah a > b ? " + hasil );   
+     
+    hasil = a < b;
+    System.out.println("Apakah a < b ? " + hasil );   
+     
+    hasil = a >= b;
+    System.out.println("Apakah a >= b ? " + hasil );   
+  
+    }
+    
+}

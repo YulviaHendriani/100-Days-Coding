bc1b00eeb3d2a70fb4a7a0fb919cb94a6ce75357
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-16 01:33:26 +1345

    Add files via upload

---
+++ Day38.java
@@ -0,0 +1,27 @@
+
+package day38;
+
+import java.util.Scanner;
+
+public class Day38 {
+
+ 
+    public static void main(String[] args) {
+        
+        Scanner input=new Scanner(System.in);
+        
+        System.out.println("Data Mahasiswa");
+        System.out.print("Masukkan nama: ");
+        String nama = input.nextLine();
+        System.out.print("Masukkan jurusan: ");
+        String jurusan = input.nextLine();
+        System.out.print("Masukkan kelas: ");
+        String kelas =input.nextLine();
+        System.out.print("Masukkan NIM: ");
+        String nim = input.nextLine();
+        
+        
+        
+    }
+    
+}

b5741443d61d78221a5099fbbbb3163c033607f6
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-22 02:43:17 +1345

    Add files via upload

---
+++ Day44.java
@@ -0,0 +1,13 @@
+
+package day44;
+public class Day44 {
+
+    public static void main(String[] args) {
+        int angka; 
+        angka=2;
+        angka*=3;
+        
+        System.out.println(angka);
+    }
+    
+}

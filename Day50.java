07f62613271353821b34dbcf64a5f71c801f455f
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-28 03:11:00 +1345

    Add files via upload

---
+++ Day50.java
@@ -0,0 +1,20 @@
+
+package day50;
+
+public class Day50 {
+
+public static void biodata(){
+    String nama ="Yulvia";
+            System.out.println("Nama: "+ nama);
+}
+ public static void alamat(){
+   String asal = "Mamasa";
+            System.out.println("Asal: "+ asal);
+}
+    public static void main(String[] args) {
+        biodata();
+        alamat();
+        
+    }
+    
+}

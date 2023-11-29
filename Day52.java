4eb7d4ec6ad3ab2fefc793df91838387b93176eb
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-11-29 22:33:27 +1345

    Add files via upload

---
+++ Day52.java
@@ -0,0 +1,18 @@
+
+package day52;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+        
+         for(int y=1; y < 100; y++){
+            System.out.println(y);
+            if(y==60){ 
+                break;
+            }
+            
+        }
+        
+    }
+    
+}

67842d08f494c8ed0d76f74627ffc079c07d242e
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-13 19:34:54 +1345

    Add files via upload

---
+++ Day65.java
@@ -0,0 +1,30 @@
+
+package day65;
+
+public class Day65 {
+
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 7;
+        boolean hasil;
+       
+        hasil = A > B;
+        System.out.println(hasil);
+       
+        hasil = A < B;
+        System.out.println(hasil);
+        
+        hasil = A >= B;
+        System.out.println(hasil);
+       
+        hasil = A <= B;
+        System.out.println(hasil);
+        
+        hasil = A == B;
+        System.out.println(hasil);
+
+        hasil = A != B;
+        System.out.println(hasil);
+    }
+    
+}
---
+++ Day66.java
@@ -0,0 +1,14 @@
+package day66;
+
+import java.util.Scanner;
+
+public class Day66 {
+
+    public static void main(String[] args) {
+        String prodi= "inFoRMaTikA";
+        String prodi1 = prodi.toLowerCase();
+        System.out.println(prodi1);
+        
+    }
+
+}

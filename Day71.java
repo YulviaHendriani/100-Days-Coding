dbcd16659d73c16c0f1df2f372c90548fb29d72a
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-19 14:52:18 +1345

    Add files via upload

---
+++ Day71.java
@@ -0,0 +1,12 @@
+
+package day71;
+
+public class Day71 {
+
+    public static void main(String[] args) {
+       String [] nama= {"Yulisa", "Hana", "Yulvia", "Mief"};
+       
+        System.out.println(nama[2]);
+    }
+    
+}

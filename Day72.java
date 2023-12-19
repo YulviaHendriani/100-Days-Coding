79ddbd9a5c1a3f101c5ba9200903a0c7fe59ca78
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-19 15:07:11 +1345

    Add files via upload

---
+++ Day72.java
@@ -0,0 +1,13 @@
+
+package day72;
+
+
+public class Day72 {
+
+   
+    public static void main(String[] args) {
+       int [] angka ={04, 06, 06};
+        System.out.println(angka[1]);
+    }
+    
+}

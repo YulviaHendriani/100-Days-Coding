4998c3f4738a8e9b7e7159bd88bd0b0810f07774
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-21 11:42:00 +1345

    Add files via upload

---
+++ Day73.java
@@ -0,0 +1,17 @@
+
+package day73;
+
+public class Day73 {
+
+    public static void main(String[] args) {
+      String kalimat = "Hal,selamat,Pagi";
+      String[] hasilsplit = kalimat.split(",");
+      for(String kata :  hasilsplit){
+          System.out.println(kata);
+          
+          //split digunakan untuk membagi kata dari sebuah kalimat
+      }
+      
+    }
+    
+}

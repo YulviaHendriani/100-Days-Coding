6c7e710506568ccf2c4986c2d8e1e3ca4f6c4a7a
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-23 20:15:09 +1345

    Add files via upload

---
+++ Day75.java
@@ -0,0 +1,14 @@
+
+package day75;
+public class Day75 {
+    public static void main(String[] args) {
+         
+        int [] y = {9,4,2,8,4};
+        int jumlah = y.length;
+        
+        for (int i = 0; i < jumlah; i++){
+            System.out.println(y[i]);
+        }
+    }
+    
+}

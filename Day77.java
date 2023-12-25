0e43b39dd386f1a8d705e7dd3a5d12a14598bba2
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-25 20:35:29 +1345

    Add files via upload

---
+++ Day77.java
@@ -0,0 +1,12 @@
+
+package day77;
+
+public class Day77 {
+
+    public static void main(String[] args) {
+        String[] dosen={"Nurdina","Abdul","Ilham","Ikhsan","Fahmi"};
+        System.out.println(dosen[2]);
+        }
+    
+    
+}

8c2907fd144cde7cc5964e56f917008941cc0683
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-27 15:36:31 +1345

    Add files via upload

---
+++ Day79.java
@@ -0,0 +1,15 @@
+
+package day79;
+
+public class Day79 {
+
+    public static void main(String[] args) {
+        String [] nama ={"Hana", "Tasya", "Lisa", "Mief", "Melisa"};
+        int jumlah = nama.length;
+        
+        for (int i = 0; i < jumlah; i++){
+            System.out.println(nama[i]);
+        }
+    }
+    
+}

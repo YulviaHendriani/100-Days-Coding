83d8cc4f02b4a6317a2c3a35f23f54d5c7c2962b
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-27 15:39:23 +1345

    Add files via upload

---
+++ Day80.java
@@ -0,0 +1,18 @@
+
+package day80;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+       String [] nama ={"Tiara", "Jelin", "Yulisa", "Yohana", "Melisa"};
+        int jumlah = nama.length;
+        
+        for (int i = 0; i < jumlah; i++){
+            System.out.println(nama[i]);
+        }
+        System.out.println(" ");
+        System.out.println("Nama pertama: " + nama[0]);
+        System.out.println("Nama kedua: " + nama[1]);
+    }
+    
+}

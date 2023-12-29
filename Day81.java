b468111331ed7772bacb8d4106e60bce0ecedb96
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-30 01:02:50 +1345

    Add files via upload

---
+++ Day81.java
@@ -0,0 +1,15 @@
+
+package day81;
+
+public class Day81 {
+
+    public static void main(String[] args) {
+         int [] y = {5, 6, 7, 8, 9};
+        int jumlah = y.length;
+        
+        for (int i = 0; i < jumlah; i++){
+            System.out.println(y[i]);
+        } 
+    }
+    
+}

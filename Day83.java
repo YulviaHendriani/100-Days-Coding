4d12be7748136338d6cbc3b77674d5f61db70eb9
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-30 06:19:38 +0800

    Add files via upload

---
+++ Day83.java
@@ -0,0 +1,15 @@
+
+package day83;
+
+public class Day83 {
+
+    public static void main(String[] args) {
+      int [] y = {5, 8, 7, 9, 10};
+        
+         for(int i = 1; i < y[1]; i++){
+            System.out.println(i); 
+        }
+       
+    }
+    
+}

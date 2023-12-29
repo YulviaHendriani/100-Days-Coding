d78e764e7f7fbedae1f53f35e0d263da73117c8d
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2023-12-30 01:07:11 +1345

    Add files via upload

---
+++ Day82.java
@@ -0,0 +1,16 @@
+
+package day82;
+
+public class Day82 {
+
+    public static void main(String[] args) {
+        int [] y = {5, 8, 7, 9, 10};
+        int jumlah = y.length;
+        
+        for (int i = 0; i < jumlah; i++){
+            System.out.println(y[i]);
+        } 
+        System.out.println("Indeks 1: "+ y[0]);
+    }
+    
+}

2b9479a5e4252ebba7ae9ebc040a7d77e42bc2dd
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-01 18:12:34 +0800

    Add files via upload

---
+++ Day84.java
@@ -0,0 +1,16 @@
+
+package day84;
+
+public class Day84 {
+
+    public static void main(String[] args) {
+        String nama = "Yulvia Hendriani";
+        String[]split = nama.split(" ");
+        String namadepan = split [0];
+        String namabelakang = split [1];
+        System.out.println("Nama Depan: "+namadepan+"\nNama Belakang: "+namabelakang);
+        System.out.println("("+nama+")");
+        
+    }
+    
+}

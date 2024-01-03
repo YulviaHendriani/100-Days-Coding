d48f1dd7b40dfd0cc3732aa39aa70b9850f331b9
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-03 11:41:50 +0800

    Add files via upload

---
+++ Day86.java
@@ -0,0 +1,16 @@
+
+package day86;
+
+public class Day86 {
+
+    public static void main(String[] args) {
+      int [] y = {8, 9, 7, 6, 2};
+        int jumlah = y.length;
+        
+        for (int i = 0; i < jumlah; i++){
+            System.out.println(y[i]);
+        } 
+        System.out.println("Indeks 3: "+ y[2]);
+    }
+    
+}

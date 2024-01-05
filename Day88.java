69f721d10169cec67af68bfbf466ea2cdae8f406
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-05 11:12:11 +0800

    Add files via upload

---
+++ Day88.java
@@ -0,0 +1,12 @@
+
+package day88;
+
+public class Day88 {
+  public static void yulvia(String nama){
+      System.out.println("Nama saya: " + nama );
+  }
+    public static void main(String[] args) {
+    yulvia ("Yulvia");
+    }
+    
+}

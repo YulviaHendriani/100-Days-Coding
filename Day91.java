75365ba9528b5600bb2ceaf89656eebe095b4406
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-07 09:53:50 +0800

    Add files via upload

---
+++ Day91.java
@@ -0,0 +1,16 @@
+
+package day91;
+
+public class Day91 {
+
+    public static void main(String[] args) {
+        String nama ="Yulvia";
+        char hurufawal = nama.charAt(0);
+        char hurufakhir = nama.charAt(5);
+        
+        System.out.println(hurufawal);
+        System.out.println(hurufakhir);
+                
+    }
+    
+}

d5dc9241746e2e56ba4f25a584ebab79e599e6a1
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-09 09:58:23 +0800

    Add files via upload

---
+++ Day92.java
@@ -0,0 +1,19 @@
+
+package day92;
+
+public class Day92 {
+
+    public static void main(String[] args) {
+    int [][] angka = {
+        {7,9,4,2},
+        {2,9,7,3}
+            
+    };
+    for (int[] baris:angka){
+        for(int elemen:baris){
+            System.out.println(elemen+" ");
+        }
+    }
+  }
+    
+}

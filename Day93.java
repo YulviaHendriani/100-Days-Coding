5e144252beaead4f1e86abbaf8a8f8b2342b78d7
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-09 09:59:30 +0800

    Add files via upload

---
+++ Day93.java
@@ -0,0 +1,20 @@
+
+package day93;
+
+import java.util.ArrayList;
+
+public class Day93 {
+
+    public static void main(String[] args) {
+        ArrayList data = new ArrayList<>();
+        data.add(1);
+        data.add("Yulvia");
+        data.add('Y');
+        data.add(150.7);
+        
+        
+        System.out.println(data);
+        
+  }
+
+}

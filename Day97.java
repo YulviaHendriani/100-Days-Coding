e1951720e51e5c7d012afe30292cb755dd78b8dd
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-13 07:06:18 +0800

    Add files via upload

---
+++ Day97.java
@@ -0,0 +1,15 @@
+
+package day97;
+
+public class Day97 {
+
+    public static void main(String[] args) {
+        int [] y = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int jumlah = y.length;
+        
+        for (int i = 9; i >=0; i--){
+            System.out.println(y[i]);
+        } 
+    }
+    
+}

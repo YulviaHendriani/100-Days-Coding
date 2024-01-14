f9becc1939238b5b39a280dece226b9064f7b0ad
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-14 11:40:40 +0800

    Add files via upload

---
+++ Day98.java
@@ -0,0 +1,23 @@
+
+package day98;
+
+import java.util.ArrayList;
+
+public class Day98 {
+
+    public static void main(String[] args) {
+        ArrayList angka = new ArrayList <>();
+        
+        angka.add(1);
+        angka.add(2);
+        angka.add(3);
+        angka.add(4);
+        angka.add(5);
+        angka.add(6);
+        
+        System.out.println(angka);
+        
+        System.out.println("indeks  5: " + angka.get(5));
+    }
+    
+}

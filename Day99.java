13ea5a8c8c741d913aeef40afeca43fc3da35be2
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-15 13:34:10 +0800

    Add files via upload

---
+++ Day99.java
@@ -0,0 +1,20 @@
+
+package day99;
+
+public class Day99 {
+
+    public static void main(String[] args) {
+   
+    int [][] angka = {
+        {1,2,3,4},
+        {5,6,7,8}
+            
+    };
+    for (int[] baris:angka){
+        for(int elemen:baris){
+            System.out.println(elemen+" ");
+        }
+    }
+    }
+    
+}

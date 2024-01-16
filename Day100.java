8f77c414d006a866b57da6fbce824087cdea9db7
YulviaHendriani <147060057+YulviaHendriani@users.noreply.github.com>
2024-01-16 08:31:54 +0800

    Add files via upload

---
+++ Day100.java
@@ -0,0 +1,17 @@
+
+package day100;
+
+public class Day100 {
+
+    public static void main(String[] args) {
+        String [][] biodata = {
+            {"Nama", "NIM",},
+            {"Yulvia", "D0223301"}
+                
+        };
+        System.out.println(biodata [0][0]+": " + biodata [1][0]);
+        System.out.println(biodata [0][1]+": "+biodata [1][1]);
+        
+    }
+    
+}
